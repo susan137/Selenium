8802fd2d39bb98ccd678cab73b07c72974a088e4
gssusangeorge <gssusangeorge@gmail.com>
2022-01-03 19:01:59 +1300

    first commit

--- selenium/src/main/java/selenium/TestGit.java
+++ selenium/src/main/java/selenium/TestGit.java
@@ -3,7 +3,7 @@ package selenium;
 public class TestGit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hai");// TODO Auto-generated method stub
 
 	}
 
