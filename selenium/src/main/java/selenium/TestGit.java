a21c1baa2fa4662b3510fc195908d54230c021c4
gssusangeorge <gssusangeorge@gmail.com>
2022-01-04 14:09:29 +1300

    My second commit

--- selenium/src/main/java/selenium/TestGit.java
+++ selenium/src/main/java/selenium/TestGit.java
@@ -3,7 +3,9 @@ package selenium;
 public class TestGit {
 
 	public static void main(String[] args) {
-		System.out.println("Hai");// TODO Auto-generated method stub
+		System.out.println("Hai");
+		System.out.println("Haii");
+		
 
 	}
 
