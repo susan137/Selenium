2066f5f76b373b06de6e24ee0b9a51944bb1fa05
gssusangeorge <gssusangeorge@gmail.com>
2022-01-05 16:07:59 +1300

    Checkdays pgm with testng

--- selenium/src/main/java/program/checkdays/CheckdaysMain.java
+++ selenium/src/main/java/program/checkdays/CheckdaysMain.java
@@ -1,11 +1,14 @@
 package program.checkdays;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class CheckdaysMain {
 	
 		static Checkdays obj=new Checkdays();
 	
 	 
-		public static void main(String[] args) {
+		/*public static void main(String[] args) {
 		
 				
 			testcase1();
@@ -18,8 +21,8 @@ public class CheckdaysMain {
 			testcase8();
 			testcase9();
 			testcase10();
-		}
-			
+		}*/
+		@Test
 		//TestCase#1:Verify with testData '0'
 		public static void testcase1() {
 			
@@ -27,138 +30,112 @@ public class CheckdaysMain {
 			
 			String actual_result=obj.checkdays(testdata);
 			String expected_result="Invalid";
-			if(actual_result.equals(expected_result)) {
-				System.out.println("Tescase#1:Test Pass");
-			}
+			Assert.assertEquals(actual_result,expected_result,"Testcase#1");
+			
 			
 		}
 		//TestCase#2:Verify with negative number
+		@Test
 		public static void testcase2() {
 			
 			int testdata=-1;
 			
 			String actual_result=obj.checkdays(testdata);
 			String expected_result="Invalid";
-			if(actual_result.equals(expected_result)) {
-				System.out.println("Tescase#2:Test Pass");
-			}
+			Assert.assertEquals(actual_result,expected_result,"Testcase#2");
 				
 		}
 		
 		//TestCase#3:Verify with Large number
+		@Test
 		public static void testcase3() {
 					
-			int testdata=-1;
+			int testdata=1354546657;
 					
 			String actual_result=obj.checkdays(testdata);
 			String expected_result="Invalid";
-			if(actual_result.equals(expected_result)) {
-				System.out.println("Tescase#3:Test Pass");
-			}
+			Assert.assertEquals(actual_result,expected_result,"Testcase#3");
 						
 		}
 		
 		//TestCase#4:Verify with TestData "1"
+		@Test
 		public static void testcase4() {
 							
 			int testdata=1;
 							
 			String actual_result=obj.checkdays(testdata);
-			String expected_result="Monday";
-			if(actual_result.equals(expected_result)) {
-				System.out.println("Tescase#4:Test Pass");
-			}
+			String expected_result="Monday2";
+			Assert.assertEquals(actual_result,expected_result,"Testcase#4");
 								
 		}
 		
 		//TestCase#5:Verify with TestData "2"
+		@Test
 		public static void testcase5() {
 									
 			int testdata=2;
 									
 			String actual_result=obj.checkdays(testdata);
 			String expected_result="Tuesday";
-			if(actual_result.equals(expected_result)) {
-				
-				System.out.println("Tescase#5:Test Pass");
-			}
+			Assert.assertEquals(actual_result,expected_result,"Testcase#5");
 										
 		}
 		
 		//TestCase#6:Verify with TestData "3"
+		@Test
 		public static void testcase6() {
 									
 					
 			int testdata=3;						
 			String actual_result=obj.checkdays(testdata);
 			String expected_result="Wednesday";
-			if(actual_result.equals(expected_result)) {
-						
-				System.out.println("Tescase#6:Test Pass");
-					
-			}
+			Assert.assertEquals(actual_result,expected_result,"Testcase#6");
 										
 				
 		}
 		//TestCase#7:Verify with TestData "4"
+		@Test
 		public static void testcase7() {
 									
 			int testdata=4;
 			String actual_result=obj.checkdays(testdata);
 			String expected_result="Thursday";
-			if(actual_result.equals(expected_result)) {
-					
-				System.out.println("Tescase#7:Test Pass");
-					
-			}
+			Assert.assertEquals(actual_result,expected_result,"Testcase#7");
 										
 		}
 		//TestCase#8:Verify with TestData "5"
+		@Test
 		public static void testcase8() {
 									
 			int testdata=5;
 			String actual_result=obj.checkdays(testdata);
 			String expected_result="Friday";
-			if(actual_result.equals(expected_result)) {
-				
-				System.out.println("Tescase#8:Test Pass");
-				
-			}
-										
+			Assert.assertEquals(actual_result,expected_result,"Testcase#8");						
 		}
 				
 		//TestCase#9:Verify with TestData "6"
+		@Test
 	    public static void testcase9() {
 									
 				int testdata=6;
 				String actual_result=obj.checkdays(testdata);
 				String expected_result="Saturday";
 					
-				if(actual_result.equals(expected_result)) {
-						
-					System.out.println("Tescase#9:Test Pass");
-					
-					
-				}
+				Assert.assertEquals(actual_result,expected_result,"Testcase#9");
 										
 				
 	    }
 			
 	 
 	    //TestCase#10:Verify with TestData "7"
-				
-	    public static void testcase10() {
-									
+		@Test
+		public static void testcase10() {
 					
 	    	int testdata=7;
 			String actual_result=obj.checkdays(testdata);
 			String expected_result="Sunday";
-			if(actual_result.equals(expected_result)) {
-						
-				System.out.println("Tescase#10:Test Pass");
-				
-					
-			}
+			Assert.assertEquals(actual_result,expected_result,"Testcase#10");
 										
 				
 	    }
